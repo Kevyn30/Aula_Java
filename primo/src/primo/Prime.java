6dcc9e2b8ebbf235c388a4d0d9d97ea7089e17b1
Kevyn30 <kevyn.nascimento@aluno.ce.gov.br>
2024-11-04 13:36:54 -0300

    pokémom2

--- primo/src/primo/Prime.java
+++ primo/src/primo/Prime.java
@@ -4,30 +4,32 @@ import java.util.Scanner;
 
 public class Prime {
 	public static void main(String[] arg) {
-		int num, rest, prime = 0;
+		isPrime();
+	}
 
+	public static void isPrime() {
+		int num, rest, prime=0;
+		
 		Scanner dig = new Scanner(System.in);
-
-		System.out.print("Digite o número inicial: ");
-
+		
+		System.out.print("Digite algum número pra saber se é primo ou n.");
+		
 		num = dig.nextInt();
-
+		
 		rest = num;
-
 		for (int i = num; i <= rest; i++) {
+
 			for (int j = 1; j <= i; j++) {
 				if (i % j == 0) {
 					prime++;
 				}
+				
 			}
 			if (prime == 2) {
 				System.out.println("O número " + i + " é primo");
 			} else {
 				System.out.println("Número " + i + " não é primo");
 			}
-			prime = 0;
 		}
-		dig.close();
 	}
-
 }
